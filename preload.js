10879b676ff2afbe8781b8274d090e3bc3c84e93
Alfred <59073967+slimer37@users.noreply.github.com>
2023-01-29 20:26:54 -0800

    Restyle (again) and shrink header on scroll (again)

--- preload.js
+++ preload.js
@@ -10,4 +10,17 @@ $(function () {
     loadHtml("/header.html", function (data) { $('body').prepend(data); });
     loadHtml("/footer.html", function (data) { $('body').append(data); });
     loadHtml("/favicons.html", function (data) { $('head').append(data); });
+
+    window.onscroll = scrollFunction;
 });
+
+function scrollFunction() {
+    const scrollThreshold = 50;
+    let header = document.getElementById("header");
+
+    if (document.body.scrollTop > scrollThreshold || document.documentElement.scrollTop > scrollThreshold) {
+        header.className = "shrunk-header";
+    } else {
+        header.className = "";
+    }
+}
