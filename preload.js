7b171c9a49faa979324dabef94b2506d9d41a14b
Alfred <59073967+slimer37@users.noreply.github.com>
2023-01-29 19:31:50 -0800

    Autoload favicons via js

--- preload.js
+++ preload.js
@@ -9,4 +9,5 @@ function loadHtml(url, action) {
 $(function () {
     loadHtml("/header.html", function (data) { $('body').prepend(data); });
     loadHtml("/footer.html", function (data) { $('body').append(data); });
+    loadHtml("/favicons.html", function (data) { $('head').append(data); });
 });
