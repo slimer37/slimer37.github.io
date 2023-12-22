6a843896954e647505f85e57819f069e42bcdc88
Alfred <59073967+slimer37@users.noreply.github.com>
2023-12-21 21:33:31 -0800

    move Home to routes

--- slimer37.github.io/src/Root.tsx
+++ slimer37.github.io/src/Root.tsx
@@ -1,7 +1,7 @@
 import Header from './components/Header.tsx'
 import Footer from './components/Footer.tsx'
 
-import Home from './Home.tsx'
+import Home from './routes/Home.tsx'
 
 function Root() {
   return (
