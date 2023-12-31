6661c206c10af036c9e721b45a3598b0d2e0feb4
Alfred <59073967+slimer37@users.noreply.github.com>
2023-12-30 16:28:21 -0800

    condense gallery tiles into components and fix links

--- src/routes/Gallery.tsx
+++ src/routes/Gallery.tsx
@@ -1,66 +1,70 @@
+import { ReactNode } from 'react'
 import './gallery.css'
+
+type ArtTileProps = {
+  url: string,
+  inner: ReactNode
+}
+
+function ArtTile(props: ArtTileProps) {
+  let url = "gallery/" + props.url
+
+  return <a href={url} className="art-tile" style={{ backgroundImage: `url(${url})` }}>
+    {props.inner}
+  </a>
+}
+
+type VideoTileProps = {
+  url: string,
+  inner: ReactNode,
+  controls?: boolean
+}
+
+function VideoTile(props: VideoTileProps) {
+  let url = "gallery/" + props.url
+
+  return <a href={url} className="art-tile">
+    {
+      props.controls ?
+        <video controls loop muted>
+          <source src={url} type="video/mp4" />
+        </video>
+        :
+        <video autoPlay loop muted>
+          <source src={url} type="video/mp4" />
+        </video>
+    }
+
+    <div className="over-video">{props.inner}</div>
+  </a>
+}
 
 function Gallery() {
   document.title = "Gallery"
-  
+
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>3D Art Gallery</h1>
       <h3 style={{ textAlign: 'center' }}>Click on any square to view the original image.</h3>
       <div className="gallery-grid">
-        <a href="mobboss.mp4" className="art-tile">
-          <video autoPlay loop muted>
-            <source src="gallery/mobboss.mp4" type="video/mp4" />
-          </video>
-          <div className="over-video">Pokémon Mafia</div>
-        </a>
-        <a href="boxcutter.jpg" className="art-tile" style={{ backgroundImage: "url(/gallery/boxcutter.jpg)" }}><p><q>Boxcutter</q> Boss</p></a>
-        <a href="gramophone.png" className="art-tile" style={{ backgroundImage: "url(/gallery/gramophone.png)", backgroundSize: '120%' }}>Gramophone</a>
-        <a href="spacerotate.mp4" className="art-tile">
-          <video autoPlay loop muted>
-            <source src="gallery/spacerotate.mp4" type="video/mp4" />
-          </video>
-          <div className="over-video">Personality Cores</div>
-        </a>
-        <a href="venom.png" className="art-tile" style={{ backgroundImage: "url(/gallery/venom.png)" }}>Venom</a>
-        <a href="room.jpg" className="art-tile" style={{ backgroundImage: "url(/gallery/room.jpg)" }}>Bedroom Replica</a>
-        <a href="burgerlooped.mp4" className="art-tile">
-          <video autoPlay loop muted>
-            <source src="gallery/burgerlooped.mp4" type="video/mp4" />
-          </video>
-          <div className="over-video">Burger Place</div>
-        </a>
-        <a href="skullresult.jpg" className="art-tile" style={{ backgroundImage: "url(/gallery/skullresult.jpg)" }}>Low-Poly T-Rex Skull</a>
-        <a href="gladosdone.jpg" className="art-tile" style={{ backgroundImage: "url(/gallery/gladosdone.jpg)" }}>GLaDOS</a>
-        <a href="bigdipper.jpg" className="art-tile" style={{ backgroundImage: "url(/gallery/bigdipper.jpg)" }}>Big Dipper</a>
-        <a href="opensign.png" className="art-tile" style={{ backgroundImage: "url(/gallery/opensign.png)" }}>Open Sign</a>
-        <a href="motiontrack.mp4" className="art-tile">
-          <video autoPlay loop muted>
-            <source src="gallery/motiontrack.mp4" type="video/mp4" />
-          </video>
-          <div className="over-video">VFX Test</div>
-        </a>
-        <a href="factorio.mp4" className="art-tile">
-          <video autoPlay loop muted>
-            <source src="gallery/factorio.mp4" type="video/mp4" />
-          </video>
-          <div className="over-video">Factorio Inserters</div>
-        </a>
-        <a href="firelegwalk.mp4" className="art-tile">
-          <video controls loop muted>
-            <source src="gallery/firelegwalk.mp4" type="video/mp4" />
-          </video>
-          <div className="over-video">Fireleg (Click to play)</div>
-        </a>
-        <a href="buildingnight.png" className="art-tile" style={{ backgroundImage: "url(/gallery/buildingnight.png)" }}>City Corner Building</a>
-        <a href="funky.jpg" className="art-tile" style={{ backgroundImage: "url(/gallery/funky.jpg)" }}><q>Funky Abstract</q></a>
-        <a href="ship.mp4" className="art-tile">
-          <video autoPlay loop muted>
-            <source src="gallery/ship.mp4" type="video/mp4" />
-          </video>
-          <div className="over-video">Ship</div>
-        </a>
-        <a href="lemon.png" className="art-tile" style={{ backgroundImage: "url(/gallery/lemon.png)" }}>Combustible Lemon</a>
+        <VideoTile url="mobboss.mp4" inner={<>Pokémon Mafia</>} />
+        <ArtTile url="boxcutter.jpg" inner={<><q>Boxcutter</q>&nbsp;Boss</>} />
+        <ArtTile url="gramophone.png" inner={<>Gramophone</>} />
+        <VideoTile url="spacerotate.mp4" inner={<>Personality Cores</>} />
+        <ArtTile url="venom.png" inner={<>Venom</>} />
+        <ArtTile url="room.jpg" inner={<>Bedroom Replica</>} />
+        <VideoTile url="burgerlooped.mp4" inner={<>Burger Place</>} />
+        <ArtTile url="skullresult.jpg" inner={<>Low-Poly T-Rex Skull</>} />
+        <ArtTile url="gladosdone.jpg" inner={<>GLaDOS</>} />
+        <ArtTile url="bigdipper.jpg" inner={<>Big Dipper</>} />
+        <ArtTile url="opensign.png" inner={<>Open Sign</>} />
+        <VideoTile url="motiontrack.mp4" inner={<>VFX Test</>} />
+        <VideoTile url="factorio.mp4" inner={<>Factorio Inserters</>} />
+        <VideoTile url="firelegwalk.mp4" inner={<>Fireleg (Click to play)</>} controls />
+        <ArtTile url="buildingnight.png" inner={<>City Corner Building</>} />
+        <ArtTile url="funky.jpg" inner={<q>Funky Abstract</q>} />
+        <VideoTile url="ship.mp4" inner={<>Ship</>} />
+        <ArtTile url="lemon.png" inner={<q>Combustible Lemon</q>} />
       </div>
     </>
   )
