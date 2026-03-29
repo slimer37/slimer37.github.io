b9b8543e78f37bf31eaf8969d3abdab1d96b2167
Alfred Roy M <59073967+slimer37@users.noreply.github.com>
2026-03-29 04:46:20 -0700

    refactor tool logos as array

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,20 @@
 import ToolLogo from '../components/ToolLogo'
 import './home.css'
 
-import unity_logo from '../assets/logos/unity_logo.svg';
+import unityLogo from '../assets/logos/unity_logo.svg';
 
 function Home() {
   document.title = "Home"
+
+  const toolInfos = [
+    { logo: unityLogo, label: "Unity (since 2019)", link: "https://unity.com/" },
+    { logo: "https://resources.jetbrains.com/storage/products/company/brand/logos/Rider_icon.svg", label: "Jetbrains Rider for Unity and other C# development", link: "https://www.jetbrains.com/rider/" },
+    { logo: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Blender_logo_no_text.svg", label: "Blender for 3D models", link: "https://www.blender.org/" },
+    { logo: "https://upload.wikimedia.org/wikipedia/commons/c/cf/Affinity_%28App%29_Logo.svg", label: "Affinity for icons, logos, and some texturing", link: "https://www.affinity.studio/" },
+    { logo: "https://upload.wikimedia.org/wikipedia/commons/6/69/Logo_Aseprite.svg", label: "Aseprite for pixel art", link: "https://www.aseprite.org/" },
+    { logo: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg", label: "VSCode to edit blog posts, websites, Python, or for simpler projects", link: "https://code.visualstudio.com/" },
+    { logo: "https://upload.wikimedia.org/wikipedia/commons/2/20/Visual_Studio_Icon_2026.svg", label: "Visual Studio for C++", link: "https://visualstudio.microsoft.com/" }
+  ];
 
   return (
     <>
@@ -16,13 +26,9 @@ function Home() {
         <h3>Tools I use:</h3>
 
         <ul className="tool-list">
-          <ToolLogo logo={unity_logo} label="Unity (since 2019)" link="https://unity.com/" />
-          <ToolLogo logo="https://resources.jetbrains.com/storage/products/company/brand/logos/Rider_icon.svg" label="Jetbrains Rider for Unity and other C# development" link="https://www.jetbrains.com/rider/" />
-          <ToolLogo logo="https://upload.wikimedia.org/wikipedia/commons/0/0c/Blender_logo_no_text.svg" label="Blender for 3D models" link="https://www.blender.org/" />
-          <ToolLogo logo="https://upload.wikimedia.org/wikipedia/commons/c/cf/Affinity_%28App%29_Logo.svg" label="Affinity for icons, logos, and some texturing" link="https://www.affinity.studio/" />
-          <ToolLogo logo="https://upload.wikimedia.org/wikipedia/commons/6/69/Logo_Aseprite.svg" label="Aseprite for pixel art" link="https://www.aseprite.org/" />
-          <ToolLogo logo="https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg" label="VSCode to edit blog posts, websites, Python, or for simpler projects" link="https://code.visualstudio.com/" />
-          <ToolLogo logo="https://upload.wikimedia.org/wikipedia/commons/2/20/Visual_Studio_Icon_2026.svg" label="Visual Studio for C++" link="https://visualstudio.microsoft.com/" />
+          {toolInfos.map(tool => (
+            <ToolLogo logo={tool.logo} label={tool.label} link={tool.link} />
+          ))}
         </ul>
       </div >
       <br />
